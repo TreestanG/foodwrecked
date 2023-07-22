3645aab2b8ce02862339ddf90b4d88cda2301703
Koolwiza <tristangee@gmail.com>
2023-07-21 23:23:11 -0700

    added animations for gradients

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,46 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'uni-green': '#6B9559'
+      },
+      animation: {
+        'gradient-x': 'gradient-x 30s ease infinite',
+        'gradient-y': 'gradient-y 30s ease infinite',
+        'gradient-xy': 'gradient-xy 30s ease infinite',
+      },
+      keyframes: {
+        'gradient-y': {
+          '0%, 100%': {
+            'background-size': '400% 400%',
+            'background-position': 'center top'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'center center'
+          }
+        },
+        'gradient-x': {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          }
+        },
+        'gradient-xy': {
+          '0%, 100%': {
+            'background-size': '400% 400%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          }
+        }
+      }
     },
   },
   plugins: [],
