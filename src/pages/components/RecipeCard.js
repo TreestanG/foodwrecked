5666ff2bacd3d0ee126b64d9ccb0a5881b0200d0
Koolwiza <tristangee@gmail.com>
2023-07-24 15:29:21 -0700

    added images for recipe cards, added href links

--- src/pages/components/RecipeCard.js
+++ src/pages/components/RecipeCard.js
@@ -11,11 +11,13 @@ function handleClick() {
 export default function RecipeCard({ recipe }) {
 
     return (
-        <Link href='/'>
-            <div className="flex-col sm:flex-row w-[24rem] m-8 p-4 rounded-lg bg-slate-100 hover:cursor-pointer hover:drop-shadow-lg" onClick={handleClick}>
-                <div>
+        <Link href={recipe.links}>
+            <div className="flex flex-col sm:flex-row  py-4 hover:cursor-pointer hover:drop-shadow-lg" onClick={handleClick}>
+
+                <img src={recipe.image} className="w-48 h-48 object-top object-cover"></img>
+                <div className="rounded-r-lg py-4 bg-slate-100 w-[24rem] px-4">
                     <div className="flex items-baseline justify-between">
-                        <h1 className="text-xl font-semibold">{recipe.title}</h1>
+                        <h1 className="text-xl font-semibold">{recipe.name}</h1>
                         <p className="font-medium">{recipe.cuisine}</p>
                     </div>
                     <p className="text-sm">{recipe.meal}</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,69 +1,17 @@
 import Image from 'next/image'
 import RecipeCard from './components/RecipeCard'
-import fs from 'fs'
-
-let recipes = [
-  {
-    "cuisine": "Southern",
-    "link": "https://www.google.com/url?q=http://www.food.com/recipe/almost-boston-market-creamed-spinach-77206&sa=D&source=editors&ust=1690137462632765&usg=AOvVaw2SCTCCHB5pqzVltay287cY",
-    "meal": "Side",
-    "protein": "None",
-    "serving": "4",
-    "title": "Almost Boston Market Creamed Spinach"
-  },
-  {
-    "cuisine": "Mediterranean",
-    "link": "https://www.google.com/url?q=http://minimalistbaker.com/simple-baba-ganoush/&sa=D&source=editors&ust=1690137462633264&usg=AOvVaw1-MV6IW0etl2WPwS6PLyAA",
-    "meal": "Condiment",
-    "protein": "none",
-    "serving": "4",
-    "title": "Baba Ganoush"
-  },
-  {
-    "cuisine": "American",
-    "link": "https://www.google.com/url?q=http://www.skinnytaste.com/bacon-wrapped-green-bean-bundles/&sa=D&source=editors&ust=1690137462633765&usg=AOvVaw3e80QxOrxK01vqOM7fepYn",
-    "meal": "Side",
-    "protein": "Bacon",
-    "serving": "4",
-    "title": "Bacon-wrapped Green Bean Bundles"
-  },
-  {
-    "cuisine": "Mediterranean",
-    "link": "https://www.google.com/url?q=http://www.myrecipes.com/recipe/bacon-onion-brown-lentil-skillet&sa=D&source=editors&ust=1690137462637893&usg=AOvVaw2ywlqy7C3ZuFAg_Rasqyli",
-    "meal": "Entr\u00e9e",
-    "protein": "Bacon",
-    "serving": "4",
-    "title": "Bacon, Onion, and Brown Lentil Skillet"
-  },
-  {
-    "cuisine": "American",
-    "link": "https://www.google.com/url?q=http://www.skinnytaste.com/2011/04/healthy-baked-chicken-nuggets.html&sa=D&source=editors&ust=1690137462638686&usg=AOvVaw20gWEoKIvJ0Tm3870Va2eB",
-    "meal": "Protein",
-    "protein": "Chicken breast",
-    "serving": "4",
-    "title": "Baked Chicken Nuggets"
-  }
-]
+import Recipes from '../../spreadsheet_recipes.json'
 
 export default function Home() {
-  data = []
-  fs.createReadStream('./spreadsheet_data.csv')
-    .pipe(parse({ delimiter: ', ', columns: true, ltrim: true }))
-    .on('data', function (csvrow) {
-      data.push(csvrow)
-    })
-
-  console.log(data)
 
   return (
     <div className="flex justify-center">
       <div className="flex-col md:-translate-x-[20rem] pt-12">
         {
-          recipes.map(r => {
+          Object.values(Recipes).map(r => {
             return <RecipeCard key={r.title} recipe={r} />
           })
         }
-
       </div>
     </div>
   )
