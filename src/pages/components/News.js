9b3406715ed358ecfc97ff4e2683388659e7234d
Koolwiza <tristangee@gmail.com>
2023-07-26 15:27:26 -0700

    changed error in naming function, Header -> News

--- src/pages/components/News.js
+++ src/pages/components/News.js
@@ -4,7 +4,7 @@ import { faExclamationCircle} from '@fortawesome/free-solid-svg-icons'
 import Image from 'next/image'
 
 
-export default function Header() {
+export default function News() {
     return (
         <div className='flex p-4 justify-center items-center sticky bg-red-400 h-8'>
             <FontAwesomeIcon icon={faExclamationCircle} className='w-4 h-4 p-6' />
