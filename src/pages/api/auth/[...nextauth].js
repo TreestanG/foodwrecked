22e7bdbdd3ed1b504f6349cd4a844727fa3cf38a
Koolwiza <tristangee@gmail.com>
2023-08-06 17:19:24 -0700

    sequelize database

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,17 +1,49 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google'
+import GithubProvider from 'next-auth/providers/github'
 import Credentials from "next-auth/providers/credentials";
-import SequelizeAdapter from "@auth/sequelize-adapter";
+import { createLogger, format, transports } from "winston";
+import SequelizeAdapter, { models } from "@auth/sequelize-adapter";
 import { DataTypes, Sequelize } from "sequelize";
 
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    logging: false,
+})
 
-export const authOptions = {
+const winston = createLogger({
+    level: 'info',
+    format: format.combine(
+        format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        format.json(),
+        format.errors({ stack: true })
+    ),
+    transports: [
+        new transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new transports.File({ filename: 'logs/combined.log' })
+    ]
+})
+
+sequelize.sync()
+
+const authHandler = NextAuth({
     secret: process.env.AUTH_SECRET,
+    adapter: SequelizeAdapter(sequelize, {
+        models: {
+            Account: sequelize.define('account', {
+                ...models.Account,
+                skillLevel: DataTypes.STRING,
+                cuisine: DataTypes.STRING,
+                dietTypes: DataTypes.ARRAY(DataTypes.STRING),
+            })
+        }
+    }),
     providers: [
         Credentials({
-            name:"Credentials",
+            name: "Credentials",
             credentials: {
-                email: { label: "Email", type: "email"},
+                email: { label: "Email", type: "email" },
                 password: { label: "Password", type: "password" }
             },
             async authorize(credentials, req) {
@@ -23,7 +55,8 @@ export const authOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
-    
-}
+})
 
-export default NextAuth(authOptions)
+export default async function handler(...params) {
+    await authHandler(...params)
+}
