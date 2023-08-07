19f4f4dea50c9580a908c205281853211c538561
Koolwiza <tristangee@gmail.com>
2023-08-06 17:20:28 -0700

    changed logo, added more responsiveness

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -1,33 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faBars } from '@fortawesome/free-solid-svg-icons'
 import Link from 'next/link'
 import { useSession } from 'next-auth/react';
+import Image from 'next/image'
 
 export default function Header() {
-    const { data: session, status } = useSession()
+
+    const { data: session } = useSession()
     let loginBtn = session ? "Profile" : "Login"
 
-    if (session) {
-        return <></>
-    } else {
-        return (
-            <div className='flex p-4 justify-between items-center sticky'>
-                <a href="/"><img src='/food_wrecked.png' className='w-64 h-12'></img></a>
-                <form className='hidden sm:flex h-12 w-[30rem] items-center justify-end'>
-                    <input className='rounded-full border-1 h-8 w-full bg-slate-200 hidden sm:block md:w-1/2 lg:w-fit'></input>
-                    <FontAwesomeIcon icon={faSearch} className='w-4 h-4 absolute p-5' />
-                </form>
-                <div className='flex gap-4 md:gap-8 items-baseline'>
-                    <a href='/pages/about' className='hidden sm:block px-5 md:text-sm lg:text-lg'>About Us</a>
-                    <a href='/' className='hidden sm:block px-5 md:text-sm lg:text-lg '>Connect</a>
-                    <a href='/pages/changelog' className='hidden sm:block px-5 md:text-sm lg:text-lg'>Change Log</a>
-                    <Link href='/profile/home'><button className='hidden md:block bg-lime-600 rounded-full text-white px-6 py-3 hover:shadow-lg'>{loginBtn}</button></Link>
-                    <FontAwesomeIcon icon={faBars} className="hidden sm:hidden" />
-                </div>
+    return (
+        <div class='flex p-4 justify-between items-center sticky'>
+            <Link href="/" class="flex items-center">
+                <Image src='/chef.png' alt='chef-hat' width={50} height={50}></Image>
+                <p class="pl-4 text-3xl text-uni-brand font-light">Food Wrecked</p>
+            </Link>
+            <form class='hidden sm:flex h-12 w-[30rem] items-center justify-end'>
+                <input class='rounded-full border-1 h-8 w-full bg-slate-200 hidden sm:block md:w-1/2 lg:w-fit'></input>
+                <FontAwesomeIcon icon={faSearch} class='w-4 h-4 absolute p-5' />
+            </form>
+            <div class='flex gap-4 md:gap-8 items-baseline'>
+                <Link href='/pages/about' class='hidden sm:block px-5 md:text-sm lg:text-lg'>About Us</Link>
+                <Link href='/' class='hidden sm:block px-5 md:text-sm lg:text-lg '>Connect</Link>
+                <Link href='/pages/changelog' class='hidden sm:block px-5 md:text-sm lg:text-lg'>Change Log</Link>
+                <Link href='/profile/home'><button class='hidden md:block bg-uni-brand rounded-full text-white px-6 py-3 hover:shadow-lg'>{loginBtn}</button></Link>
+                <FontAwesomeIcon icon={faBars} class="hidden sm:hidden" />
             </div>
-        )
-    }
-
-
+        </div>
+    )
 }
