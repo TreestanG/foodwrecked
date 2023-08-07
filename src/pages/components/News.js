8a30bb9011c3367fbabee301eedf0471fff99765
Koolwiza <tristangee@gmail.com>
2023-08-06 17:19:56 -0700

    className -> class

--- src/pages/components/News.js
+++ src/pages/components/News.js
@@ -4,8 +4,8 @@ import { faExclamationCircle} from '@fortawesome/free-solid-svg-icons'
 
 export default function News() {
     return (
-        <div className='flex p-4 justify-center items-center sticky bg-red-400 h-8'>
-            <FontAwesomeIcon icon={faExclamationCircle} className='w-4 h-4 p-6' />
+        <div class='flex p-4 justify-center items-center sticky bg-red-400 h-8'>
+            <FontAwesomeIcon icon={faExclamationCircle} class='w-4 h-4 p-6' />
             <p>Please keep in mind this site is in <b>DEVELOPMENT</b>. Bugs will occur</p>
         </div>
     )
