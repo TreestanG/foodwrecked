c58507de6f4aa8c891336f392d05d974c9552416
Koolwiza <tristangee@gmail.com>
2023-08-06 17:19:37 -0700

    added keys for entries

--- src/pages/components/ChangeLogEntry.js
+++ src/pages/components/ChangeLogEntry.js
@@ -5,15 +5,15 @@ export default function RecipeCard({ logData }) {
     const fromNowDate = moment(logData.date).fromNow()
 
     return (
-        <div className="border-2 p-6 w-[32rem] rounded-lg border-slate-200 hover:border-uni-green shadow-xl">
-            <div className="flex-col sm:flex-row flex justify-between items-baseline pb-4">
-            <h1 className="text-lg font-semibold">{logData.version} - {logData.title}</h1>
-            <a className="font-light">{fromNowDate}</a>
+        <div class="border-2 p-6 w-[32rem] rounded-lg border-slate-200 hover:border-green-500 shadow-xl">
+            <div class="flex-col sm:flex-row flex justify-between items-baseline pb-4">
+            <h1 class="text-lg font-semibold">{logData.version} - {logData.title}</h1>
+            <a class="font-light">{fromNowDate}</a>
             </div>
             {
                 logData.changes.map(c => {
                     return (
-                        <p className="pl-4">{c}</p>
+                        <p class="pl-4" key={c.version}>{c}</p>
                     )
                 })
             }
