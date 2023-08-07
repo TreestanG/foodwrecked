453e50d5fd05c03a52452527ec46ff8334c45daa
Koolwiza <tristangee@gmail.com>
2023-08-06 17:21:37 -0700

    time formatter for bbcdata

---
+++ util/type_helpers.js
@@ -0,0 +1,12 @@
+export function TimeToNum(time) {
+    time = String(time)
+    let spltTime = [time.slice(0, time.length-2), time.slice(-2)]
+    let mins = parseInt(spltTime[0])
+    let sec = parseInt(spltTime[1])/60
+    let totalTime = Math.floor(mins + sec)
+    let hours = Math.floor(totalTime/60)
+    let minutes = totalTime % 60
+    return hours === 0 ? `${minutes}m` : `${hours}h ${minutes}m`
+    
+}
+
