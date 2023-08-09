963ccd3cb745da54f094c97c8c7c59392d83984c
Koolwiza <tristangee@gmail.com>
2023-08-08 21:18:25 -0700

    feat: perpage layouts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,25 +1,41 @@
 import './globals.css'
 import { SessionProvider } from 'next-auth/react'
+import { SideBarNavProvider } from './context/SideNavTabContext'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import Head from 'next/head'
-import News from './components/News'
 import Header from './components/Header'
 config.autoAddCss = false
 
 export default function MyApp({
-  Component, 
+  Component,
   pageProps: { session, ...pageProps }
 }) {
-  return (
-    <SessionProvider session={session}>
-      <Head>
-        <title>Food Wrecked</title>
-      </Head>
-      <News />
-      <Header/>
-      <Component {...pageProps} />
+  const getLayout = Component.getLayout || ((page) => page)
+  if (Component.getLayout) {
+    return getLayout(
 
-    </SessionProvider>
-  )
+      <SessionProvider session={session}>
+        <SideBarNavProvider>
+          <Head>
+            <title>Food Wrecked</title>
+          </Head>
+          <Component {...pageProps} />
+        </SideBarNavProvider>
+      </SessionProvider>
+    )
+  } else {
+    return (
+      <SessionProvider session={session}>
+        <Head>
+          <title>Food Wrecked</title>
+        </Head>
+        <Header />
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </SessionProvider>
+
+    )
+  }
 }
