7430c79f32aabeb70045f913da6a6d2c17f3b1be
Koolwiza <tristangee@gmail.com>
2023-08-08 21:22:58 -0700

    feat: add dashboard layout

---
+++ src/pages/layout/Dashboard.js
@@ -0,0 +1,11 @@
+import SideNav from "../components/dashboard/SideNav";
+
+export default function DashboardLayout({ children }) {
+    return (
+        <div className="flex">
+            <SideNav />
+            <main>{children}</main>
+        </div>
+
+    )
+}
