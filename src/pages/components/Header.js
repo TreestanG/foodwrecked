f805f241a3547644bf52efe46a6d4b6704fb5716
Koolwiza <tristangee@gmail.com>
2023-08-08 21:21:53 -0700

    refactor: removed checking session, now already provided

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -10,22 +10,25 @@ export default function Header() {
     const { data: session } = useSession()
     let loginBtn = session ? "Profile" : "Login"
 
+    if (!session) {
+        return <></>
+    }
     return (
-        <div class='flex p-4 justify-between items-center sticky'>
-            <Link href="/" class="flex items-center">
+        <div className='flex p-4 justify-between items-center'>
+            <Link href="/" className="flex items-center">
                 <Image src='/chef.png' alt='chef-hat' width={50} height={50}></Image>
-                <p class="pl-4 text-3xl text-uni-brand font-light">Food Wrecked</p>
+                <p className="pl-4 text-3xl text-uni-brand font-light">Food Wrecked</p>
             </Link>
-            <form class='hidden sm:flex h-12 w-[30rem] items-center justify-end'>
-                <input class='rounded-full border-1 h-8 w-full bg-slate-200 hidden sm:block md:w-1/2 lg:w-fit'></input>
-                <FontAwesomeIcon icon={faSearch} class='w-4 h-4 absolute p-5' />
+            <form className='hidden sm:flex h-12 w-[30rem] items-center justify-end'>
+                <input className='rounded-full border-1 h-8 w-full bg-slate-200 hidden sm:block md:w-1/2 lg:w-fit'></input>
+                <FontAwesomeIcon icon={faSearch} className='w-4 h-4 absolute p-5' />
             </form>
-            <div class='flex gap-4 md:gap-8 items-baseline'>
-                <Link href='/pages/about' class='hidden sm:block px-5 md:text-sm lg:text-lg'>About Us</Link>
-                <Link href='/' class='hidden sm:block px-5 md:text-sm lg:text-lg '>Connect</Link>
-                <Link href='/pages/changelog' class='hidden sm:block px-5 md:text-sm lg:text-lg'>Change Log</Link>
-                <Link href='/profile/home'><button class='hidden md:block bg-uni-brand rounded-full text-white px-6 py-3 hover:shadow-lg'>{loginBtn}</button></Link>
-                <FontAwesomeIcon icon={faBars} class="hidden sm:hidden" />
+            <div className='flex gap-4 md:gap-8 items-baseline'>
+                <Link href='/pages/about' className='hidden sm:block px-5 md:text-sm lg:text-lg'>About Us</Link>
+                <Link href='/' className='hidden sm:block px-5 md:text-sm lg:text-lg '>Connect</Link>
+                <Link href='/pages/changelog' className='hidden sm:block px-5 md:text-sm lg:text-lg'>Change Log</Link>
+                <Link href='/profile/home'><button className='hidden md:block bg-uni-brand rounded-full text-white px-6 py-3 hover:shadow-lg'>{loginBtn}</button></Link>
+                <FontAwesomeIcon icon={faBars} className="hidden sm:hidden" />
             </div>
         </div>
     )
