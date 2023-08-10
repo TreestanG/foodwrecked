008e5b412f106e38499fbe87dce306ae93d74095
Koolwiza <tristangee@gmail.com>
2023-08-10 11:54:47 -0700

    reformatted text

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,9 @@
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>
+    return (
+      <div className="flex justify-center items-center">
+        <h1 className="font-bold">
+          404 - Page Not Found
+        </h1>
+      </div>
+    )
   }
