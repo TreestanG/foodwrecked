db3fb650c51e54e93d2866bc366f7d2cdc480b7a
Koolwiza <tristangee@gmail.com>
2023-09-25 17:03:45 -0700

    changed design

--- src/components/dashboard/SideNavTab.js
+++ src/components/dashboard/SideNavTab.js
@@ -11,7 +11,7 @@ export default function SideNavTab({ name, icon, link }) {
     }
     return (
         <Link href={link}>
-            <button onClick={setTab} className={`flex hover:bg-slate-200 hover:cursor-pointer  w-full items-baseline p-4 pl-12 ml-6 rounded-lg ${activeTab === name ? "text-slate-950 bg-slate-200" : "text-gray-400"}`}>
+            <button onClick={setTab} className={`flex hover:bg-slate-200 hover:cursor-pointer  w-full items-baseline p-4 pl-12 ml-6 rounded-lg ${activeTab === name ? "text-slate-950 bg-gray-200" : "text-gray-400"}`}>
                 <p className={`text-xl font-medium text-center`}>{name}</p>
             </button>
         </Link>
