2195266a575c43a3e806541096ab950298a28006
Tristan <tristangee@gmail.com>
2023-11-12 14:24:55 -0800

    changing to use navlink from mantine

--- src/pages/components/dashboard/SideNav.js
+++ src/pages/components/dashboard/SideNav.js
@@ -1,28 +1,39 @@
 
-import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { SideBarNavProvider } from "@/pages/context/SideNavTabContext";
-import { BookOpenIcon, HomeIcon } from '@heroicons/react/outline'
-import SideNavTab from "./SideNavTab";
 import Image from "next/image";
+import { NavLink } from "@mantine/core";
+import { IconHome2, IconToolsKitchen2 } from "@tabler/icons-react";
+import { useState } from "react";
+
 
 let tabs = [
-    ["Home", HomeIcon, "/profile/home"],
-    ["Recipes", BookOpenIcon, "/profile/recipes"]
+    { icon: IconHome2, name: "Home", href: "/profile/home" },
+    { icon: IconToolsKitchen2, name: "My Recipes", href: "/profile/recipes" },
 ]
 
 export default function SideNav() {
+    const [active, setActive] = useState(0)
+
     return (
         <SideBarNavProvider>
-            <div className="flex-col hidden lg:flex  w-[20rem] h-screen divide-y-2 shadow-lg">
+            <div className="flex-col hidden lg:flex  w-[20rem] h-screen divide-y-2 shadow-lg mr-8">
                 <Link href="/" className="flex justify-center">
                     <Image src='/chef.png' alt='chef-hat' width={100} height={100}></Image>
                 </Link>
                 {
-                    tabs.map(tab => {
-                        return <SideNavTab key={tab[0]} name={tab[0]} icon={tab[1]} link={tab[2]} />
+                    tabs.map((tab, index) => {
+                        return <NavLink
+                            key={index}
+                            icon={<tab.icon />}
+                            label={tab.name}
+                            active={active === index}
+                            onClick={() => setActive(index)}
+                            href={tab.href}
+                            
+                            />
                     })
-                }
+                } 
             </div>
         </SideBarNavProvider>
     )
