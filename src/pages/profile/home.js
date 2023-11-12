04b44a551979da8c93c4cf94317bd7e0a3b4f944
Tristan <tristangee@gmail.com>
2023-11-12 14:25:13 -0800

    changing spacing

--- src/pages/profile/home.js
+++ src/pages/profile/home.js
@@ -4,15 +4,13 @@ import Link from 'next/link'
 import { TimeToNum } from '../../../util/type_helpers'
 import DashboardLayout from '../layout/Dashboard'
 import Image from 'next/image'
-import { SideBarNavContext } from '../context/SideNavTabContext'
-import { useContext } from 'react'
+import { NavLink } from '@mantine/core'
+import { prisma } from '../api/auth/[...nextauth]'
 
 const example_data = { 'page': { 'article': { 'author': 'Cassie Best', 'description': 'Give the classic blueberry muffin a makeover by adding a crisp maple-sugar glaze, which sets to a cracked crust', 'id': '5199401', 'tags': [] }, 'recipe': { 'collections': ['Muffin', "Valentine's Day brunch", 'Easy muffin'], 'cooking_time': 6400, 'prep_time': 1200, 'serves': 0, 'keywords': ['Baking', 'Fruit muffin recipe', 'Maple syrup cake', 'Cupcakes', 'freezer-friendly muffins'], 'ratings': 90, 'nutrition_info': ['Added sugar 21g', 'Carbohydrate 37g', 'Kcal 240 calories', 'Protein 3g', 'Salt 0.6g', 'Saturated fat 5g', 'Fat 8g'], 'ingredients': ['plain flour', 'baking powder', 'bicarbonate of soda', 'granulated sugar', 'unsalted butter', 'buttermilk', 'egg', 'maple syrup', 'vanilla extract', 'blueberry', 'icing sugar'], 'courses': ['Afternoon tea', 'Breakfast', 'Brunch', 'Treat'], 'cusine': 'American', 'diet_types': [], 'skill_level': 'Easy', 'post_dates': '1435705200' }, 'channel': 'Recipe', 'title': 'Maple-glazed blueberry muffins' } }
 
 export default function ProfileHome() {
-    const [activeTab, setActiveTab] = useContext(SideBarNavContext)
     const { data: session } = useSession()
-    setActiveTab('Home')
 
     const { cooking_time, prep_time, cusine } = example_data.page.recipe
     const { description } = example_data.page.article
@@ -23,7 +21,7 @@ export default function ProfileHome() {
     return (
         <div className="flex">
             <div className="">
-                <div className="p-8">
+                <div className="">
                     <div className="flex p-11 justify-between">
                         <div className="">
                             <h1 className="text-4xl font-semibold">Welcome, {session.user.name}</h1>
