794a0cd6830a423d0e0d81b8c66595d9ae79c380
Tristan <tristangee@gmail.com>
2023-11-20 10:41:33 -0800

    removed callbacks

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -17,14 +17,6 @@ const authHandler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
-    callbacks: {
-        async session({ session, token, user }) {
-            // Send properties to the client, like an access_token and user id from a provider.
-            session.user.id = token.id
-
-            return session
-        }
-    }
 })
 
 export default async function handler(...params) {
