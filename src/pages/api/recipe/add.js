56c7ffd8bc0c4f0d9a85e01f906aae0b5cf3ff1e
Tristan <tristangee@gmail.com>
2023-11-20 10:42:29 -0800

    add recipe end point

--- src/pages/api/recipe/add.js
+++ src/pages/api/recipe/add.js
@@ -1,18 +1,48 @@
 import { prisma } from "../auth/[...nextauth]";
 
 export default async function handle(req, res) {
-    console.log(req.body)
+    if (req.method === 'POST') {
 
-   /* if (req.method === 'POST') {
         const jsonData = req.body;
+        console.log(jsonData)
 
         try {
+            const user = await prisma.user.findUnique({
+                where: {
+                    email: jsonData.author
+                }
+            
+            })
+
+            if (!user) {
+                res.status(404).json({ error: 'User not found' });
+                return;
+            }
+
+            const account = await prisma.account.findFirst({
+                where: {
+                    userId: user.id
+                }
+            })
+
+            if (!account) {
+                res.status(404).json({ error: 'Account not found' });
+                return;
+            }
+
             const recipe = await prisma.recipe.create({
-                data: jsonData,
+                data: {
+                    ...jsonData.recipe, author: {
+                        connect: {
+                            id: account.id
+                        }
+                    }
+                },
+
             });
 
             res.status(200).json({ id: recipe.id });
-             console.log('worked')
+            console.log('worked')
         } catch (error) {
             res.status(500).json({ error: 'Unable to create recipe' });
             console.log(error)
@@ -21,5 +51,5 @@ export default async function handle(req, res) {
         }
     } else {
         res.status(405).json({ error: 'Method not allowed' });
-    } */
+    }
 }
