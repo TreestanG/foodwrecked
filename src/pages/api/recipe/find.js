c63cc473d19cdec362712fef700a5592e3e931c7
Tristan <tristangee@gmail.com>
2023-12-20 18:41:16 -0800

    find by user, or all recipes

--- src/pages/api/recipe/find.js
+++ src/pages/api/recipe/find.js
@@ -1,9 +1,26 @@
 const { prisma } = require("../auth/[...nextauth]");
 
 export default async function handle(req, res) {
-    console.log(req.body)
-    const recipes = await prisma.recipe.findMany()
-    res.json(recipes)
 
+    if (req.method === 'POST') {
+        const data = req.body
+        const id = data.id
 
+        const recipe = await prisma.recipe.findMany({
+            where: {
+                authorId: id
+            }
+        })
+        res.json(recipe)
+    } else {
+        const recipes = await prisma.recipe.findMany()
+        recipes.sort((a, b) => {
+            if (a.reviewCount === b.reviewCount) {
+                return b.rating - a.rating; // Sort by rating if review count is the same
+            }
+            return b.reviewCount - a.reviewCount; // Sort by review count
+        })
+        
+        res.json(recipes)
+    }
 }
