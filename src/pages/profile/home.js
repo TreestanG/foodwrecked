ac2f4a840fee795437810136f25c01a15d993783
Tristan <tristangee@gmail.com>
2023-12-20 18:43:04 -0800

    added recipes towards home page

--- src/pages/profile/home.js
+++ src/pages/profile/home.js
@@ -5,7 +5,8 @@ import { TimeToNum } from '../../../util/type_helpers'
 import DashboardLayout from '../layout/Dashboard'
 import Image from 'next/image'
 import { Button, ButtonGroup, NavLink } from '@mantine/core'
-import { prisma } from '../api/auth/[...nextauth]'
+import { useEffect, useState } from 'react'
+import RecipeCard from '../components/RecipeCard'
 
 const example_data = { 'page': { 'article': { 'author': 'Cassie Best', 'description': 'Give the classic blueberry muffin a makeover by adding a crisp maple-sugar glaze, which sets to a cracked crust', 'id': '5199401', 'tags': [] }, 'recipe': { 'collections': ['Muffin', "Valentine's Day brunch", 'Easy muffin'], 'cooking_time': 6400, 'prep_time': 1200, 'serves': 0, 'keywords': ['Baking', 'Fruit muffin recipe', 'Maple syrup cake', 'Cupcakes', 'freezer-friendly muffins'], 'ratings': 90, 'nutrition_info': ['Added sugar 21g', 'Carbohydrate 37g', 'Kcal 240 calories', 'Protein 3g', 'Salt 0.6g', 'Saturated fat 5g', 'Fat 8g'], 'ingredients': ['plain flour', 'baking powder', 'bicarbonate of soda', 'granulated sugar', 'unsalted butter', 'buttermilk', 'egg', 'maple syrup', 'vanilla extract', 'blueberry', 'icing sugar'], 'courses': ['Afternoon tea', 'Breakfast', 'Brunch', 'Treat'], 'cusine': 'American', 'diet_types': [], 'skill_level': 'Easy', 'post_dates': '1435705200' }, 'channel': 'Recipe', 'title': 'Maple-glazed blueberry muffins' } }
 
@@ -18,43 +19,68 @@ export default function ProfileHome() {
 
     const totalTime = TimeToNum(cooking_time + prep_time)
 
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        const fetchRecipes = async () => {
+            try {
+                const recipesData = await fetch('/api/recipe/find').then(res => res.json())
+
+                setRecipes(recipesData);
+            } catch (error) {
+                console.error('Error fetching recipes:', error);
+            }
+        };
+
+        fetchRecipes();
+    }, []);
+
+    const recipeData = recipes.map(recipe => {
+        return {
+            ...recipe, diffColor: recipe.difficulty === 'Easy' ? 'green' : recipe.difficulty === 'Intermediate' ? 'yellow' : 'red'
+        }
+    })
+
     return (
         <div className="flex">
             <div className="">
-                <div className="">
-                    <div className="flex p-11 justify-between">
-                        <div className="">
-                            <h1 className="text-4xl font-semibold">Welcome, {session.user.name}</h1>
-                            <p className=" text-gray-500">Manage your recipes, favorites, and preferred choices here!</p>
-                        </div>
-                        <div className="flex gap-16">
-                            <button onClick={signOut} className=''>Sign Out</button>
-                            <Image src={session.user.image ?? "/blank.jpg"} width={64} height={64} alt="profile" className="rounded-full" ></Image>
-                        </div>
+                <div className="flex p-11 justify-between">
+                    <div className="">
+                        <h1 className="text-4xl font-semibold">Welcome, {session.user.name}</h1>
+                        <p className=" text-gray-500">Manage your recipes, favorites, and preferred choices here!</p>
                     </div>
-                    <h1 className=" text-4xl font-medium">Recipe of the day...</h1>
-                    <Link className="cursor-pointer " href='/profile/home'>
-                        <div className="drop-shadow-lg bg-amber-700 flex justify-between rounded-3xl mt-4 transition ease-in-out delay-75 hover:scale-105 duration-200">
-                            <div className=" p-12 text-white w-[30rem]">
-                                <h1 className="font-bold text-3xl capitalize">{title}</h1>
-                                <p className="pt-4">{description}</p>
-                            </div>
-                            <div className='flex flex-col justify-between items-end p-4 text-white'>
-                                <p>{totalTime}</p>
-                                <p>{cusine}</p>
-                            </div>
-                            <img src='/muffins.jpg' className="object-cover w-80 rounded-r-3xl"></img>
+                    <div className="flex gap-16">
+                        <button onClick={signOut} className=''>Sign Out</button>
+                        <Image src={session.user.image ?? "/blank.jpg"} width={64} height={64} alt="profile" className="rounded-full" ></Image>
+                    </div>
+                </div>
+                {/* <h1 className=" text-4xl font-medium">Recipe of the day...</h1>
+                <Link className="cursor-pointer " href='/profile/home'>
+                    <div className="drop-shadow-lg bg-amber-700 flex justify-between rounded-3xl mt-4 transition ease-in-out delay-75 hover:scale-105 duration-200">
+                        <div className=" p-12 text-white w-[30rem]">
+                            <h1 className="font-bold text-3xl capitalize">{title}</h1>
+                            <p className="pt-4">{description}</p>
                         </div>
-                    </Link>
+                        <div className='flex flex-col justify-between items-end p-4 text-white'>
+                            <p>{totalTime}</p>
+                            <p>{cusine}</p>
+                        </div>
+                        <img src='/muffins.jpg' className="object-cover w-80 rounded-r-3xl"></img>
+                    </div>
+                </Link> */}
 
-                    <h1 className="text-4xl font-medium pt-8">Your Recipes</h1>
-                    <div className="flex flex-wrap p-8 ">
-                        <Button
-                            onClick={() => {
-                                fetch('/api/recipe/find')
-                            }}
-                        />
-                    </div>
+                <h1 className="text-4xl font-medium py-8">Your Recipes</h1>
+                <div className="flex flex-wrap p-3 border-2 rounded-md">
+                    {
+                        recipeData.map((recipe, index) => {
+                            return (
+                                <div className="w-1/3 p-2" key={recipe.name}>
+                                    <RecipeCard recipe={recipe} />
+                                    {(index + 1) % 3 === 0 && <div className="w-full"></div>}
+                                </div>
+                            );
+                        })
+                    }
                 </div>
             </div>
         </div>
