f7797907dd03a0f05b37c3f5177597cd376e76b0
Tristan <tristangee@gmail.com>
2023-12-20 17:15:39 -0800

    show all restaurants page

---
+++ src/pages/profile/restaurant.js
@@ -0,0 +1,123 @@
+import { getSession } from 'next-auth/react'
+import DashboardLayout from '../layout/Dashboard'
+import { prisma } from '../api/auth/[...nextauth]'
+import { useEffect, useState } from 'react'
+import { CloseButton, Input, TextInput } from '@mantine/core'
+import { IconSearch } from '@tabler/icons-react'
+import { getHotkeyHandler, useIdle } from '@mantine/hooks'
+import RestaurantCard from '../components/RestaurantCard'
+
+let demoData = {
+    id: 'y7yblnKXbLW2c-1f1qTV5Q',
+    alias: 'sushi-adachi-milpitas',
+    name: 'Sushi Adachi',
+    image_url: 'https://s3-media4.fl.yelpcdn.com/bphoto/3Vp3SQt_YjjM0uwbRdwtSg/o.jpg',
+    is_closed: false,
+    url: 'https://www.yelp.com/biz/sushi-adachi-milpitas?adjust_creative=cgvo8ZSCzO5VOtVcT2dtbw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=cgvo8ZSCzO5VOtVcT2dtbw',
+    review_count: 56,
+    categories: [{ alias: 'japanese', title: 'Japanese' }],
+    rating: 4.5,
+    coordinates: { latitude: 37.41944401163271, longitude: -121.915679 },
+    transactions: ['delivery', 'pickup'],
+    location: {
+        address1: '668 Barber Ln',
+        address2: null,
+        address3: '',
+        city: 'Milpitas',
+        zip_code: '95035',
+        country: 'US',
+        state: 'CA',
+        display_address: ['668 Barber Ln', 'Milpitas, CA 95035']
+    },
+    phone: '+14084326270',
+    display_phone: '(408) 432-6270',
+    distance: 2657.2255594925314
+}
+
+export default function ProfileHome() {
+    const [value, setValue] = useState('')
+    const idle = useIdle(2500)
+    const [businesses, setBusinesses] = useState([])
+    const [coords, setCoords] = useState({})
+
+    useEffect(() => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                setCoords({ lat: position.coords.latitude, long: position.coords.longitude })
+            });
+        }
+    }, [])
+
+    const handleSearch = () => {
+        const apiLink = `https://api.yelp.com/v3/businesses/search?latitude=${coords.lat}&longitude=${coords.long}&sort_by=best_match&limit=20&term=${value}`
+        const apiKey = `otu6V4zEoL4Jppb3tWTE8tQCV0pnf7q9Ox_NUR2wZwcX6npV8-qv0bzZV9Tp6VqFrYWG0-Wwn2IBz55XnRbCkMGzRGqBEAF8rDT5pB6Wh0QRu4d-FAFtxkKMmcZiZXYx`
+        fetch('/api/back', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ apiLink, apiKey })
+        }).then(res => res.json()).then(data => {
+            console.log(data.businesses)
+            setBusinesses(data.businesses)
+        })
+    }
+
+
+
+    return (
+        <div className='flex justify-center flex-wrap p-20'>
+            <h1 className='text-5xl font-bold '>Welcome to the restaurants page!</h1>
+            <TextInput className='w-3/4'
+                placeholder='Search for recipes'
+                leftSection={<IconSearch />}
+                variant='filled'
+                radius='lg'
+                mt={20}
+                value={value}
+                onChange={(event) => setValue(event.currentTarget.value)}
+                onKeyDown={
+                    getHotkeyHandler([
+                        ['enter', handleSearch]
+                    ])
+                }
+            />
+
+            <div className='p-12 flex flex-wrap w-1/2'>
+                {
+                    businesses ? businesses.map(business => {
+                        return <RestaurantCard restaurant={business} key={business.id} />
+                    }) : undefined
+                }
+            </div>
+        </div>)
+
+}
+
+export async function getServerSideProps(context) {
+    const session = await getSession(context)
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/api/auth/signin',
+                permanent: false
+            }
+        }
+    }
+    return {
+        props: {
+            session
+        }
+    }
+}
+
+
+ProfileHome.getLayout = function getLayout(page) {
+    return (
+        <DashboardLayout>
+            {page}
+        </DashboardLayout>
+    )
+}
+
