4207a2c66e2d66abaa38d0d73bb67e2a8398c50c
Tristan <tristangee@gmail.com>
2023-12-20 18:43:15 -0800

    sticky header

--- src/pages/layout/Dashboard.js
+++ src/pages/layout/Dashboard.js
@@ -2,7 +2,7 @@ import SideNav from "../components/dashboard/SideNav";
 
 export default function DashboardLayout({ children }) {
     return (
-        <div className="flex">
+        <div className="flex h-max overflow-y-auto">
             <SideNav />
             <main>{children}</main>
         </div>
